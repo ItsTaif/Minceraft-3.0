6be463ffe17741ec1eb153951b480564e9207ef6
BrianTeam818 <le.danh.brian@gmail.com>
2018-02-16 17:50:26 -0500

    Left and Right Auton
    The left and right auton are mostly complete

--- src/org/usfirst/frc/team818/robot/autonomi/RightAuton.java
+++ src/org/usfirst/frc/team818/robot/autonomi/RightAuton.java
@@ -3,6 +3,9 @@ package org.usfirst.frc.team818.robot.autonomi;
 import java.util.LinkedList;
 
 import org.usfirst.frc.team818.robot.commands.components.Drive4Distance;
+import org.usfirst.frc.team818.robot.commands.components.ElevatorAutonCommand;
+import org.usfirst.frc.team818.robot.commands.components.IntakeOutAutonCommand;
+import org.usfirst.frc.team818.robot.commands.components.TurnAngle;
 
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.command.CommandGroup;
@@ -64,13 +67,55 @@ public class RightAuton extends CommandGroup {
 		switch (target) {
 		case 1: //left Scale
 			addSequential(new WaitCommand(delay));
-			addSequential(new Drive4Distance(0));
+			addSequential(new Drive4Distance(235.235, 0.7));
+			addParallel(new ElevatorAutonCommand("Scale"));
+			addSequential(new WaitCommand(0.25));
+			addSequential(new TurnAngle(-90));
+			addSequential(new Drive4Distance(264, 0.7));
+			addSequential(new WaitCommand(0.25));
+			addSequential(new TurnAngle(90));
+			addSequential(new Drive4Distance(88.765));
+			addSequential(new WaitCommand(0.25));
+			addSequential(new TurnAngle(90));
+			addSequential(new Drive4Distance(42));
+			addSequential(new WaitCommand(0.25));
+			addSequential(new IntakeOutAutonCommand(0.8, 2));
 			break;
 		case 2: //rightScale
+			addSequential(new WaitCommand(delay));
+			addSequential(new Drive4Distance(324, 0.7));
+			addParallel(new ElevatorAutonCommand("Scale"));
+			addSequential(new WaitCommand(0.25));
+			addSequential(new TurnAngle(-90));
+			addSequential(new Drive4Distance(42));
+			addSequential(new WaitCommand(0.25));
+			addSequential(new IntakeOutAutonCommand(0.8, 2));
 			break;
 		case 3: //leftSwitch
+			addSequential(new WaitCommand(delay));
+			addSequential(new Drive4Distance(235.235, 0.7));
+			addParallel(new ElevatorAutonCommand("Scale"));
+			addSequential(new WaitCommand(0.25));
+			addSequential(new TurnAngle(-90));
+			addSequential(new Drive4Distance(264, 0.7));
+			addSequential(new WaitCommand(0.25));
+			addSequential(new TurnAngle(-90));
+			addSequential(new Drive4Distance(67.235));
+			addSequential(new WaitCommand(0.25));
+			addSequential(new TurnAngle(-90));
+			addSequential(new Drive4Distance(55.25));
+			addSequential(new WaitCommand(0.25));
+			addSequential(new IntakeOutAutonCommand(0.8, 2));
 			break;
 		case 4: //rightSwitch
+			addSequential(new WaitCommand(delay));
+			addSequential(new Drive4Distance(168));
+			addParallel(new ElevatorAutonCommand("Switch"));
+			addSequential(new WaitCommand(0.25));
+			addSequential(new TurnAngle(-90));
+			addSequential(new Drive4Distance(55.25));
+			addSequential(new WaitCommand(0.25));
+			addSequential(new IntakeOutAutonCommand(0.8, 2));
 			break;
 		case 5: addSequential(new DoNothing());
 			break;
