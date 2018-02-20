7534b0f434c76d38f17066d48de6412ed63f2792
ItsTaif <taif.rahman21@gmail.com>
2018-02-20 18:12:22 -0500

    Vault Auton
    asdasd

---
+++ src/org/usfirst/frc/team818/robot/autonomi/VaultAuton.java
@@ -0,0 +1,42 @@
+package org.usfirst.frc.team818.robot.autonomi;
+
+import org.usfirst.frc.team818.robot.commands.components.Drive4Distance;
+import org.usfirst.frc.team818.robot.commands.components.IntakeInAutonCommand;
+import org.usfirst.frc.team818.robot.commands.components.IntakeOutAutonCommand;
+import org.usfirst.frc.team818.robot.commands.components.TurnAngle;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+/**
+ *
+ */
+public class VaultAuton extends CommandGroup {
+
+    public VaultAuton() {
+    	
+    	addSequential(new Drive4Distance(10.0));
+    	addSequential(new TurnAngle(-20));
+    	addSequential(new Drive4Distance(10.0));
+    	addSequential(new TurnAngle(-70));
+    	addSequential(new Drive4Distance(18.0));
+    	addSequential(new IntakeOutAutonCommand(0.5, 2.0));
+    	addSequential(new Drive4Distance(-10.0));
+    	addSequential(new TurnAngle(-160));
+    	addSequential(new Drive4Distance(10.0));
+    	addSequential(new TurnAngle(-20));
+    	addSequential(new Drive4Distance(20.0));
+    	addParallel(new IntakeInAutonCommand(1.0, 2.0));
+    	addSequential(new Drive4Distance(-10.0));
+    	addSequential(new TurnAngle(-160));
+    	addSequential(new Drive4Distance(10.0));
+    	addSequential(new TurnAngle(-20));
+    	addSequential(new Drive4Distance(20.0));
+    	addSequential(new IntakeOutAutonCommand(0.5, 2.0));
+    	addSequential(new Drive4Distance(-10.0));
+    	addSequential(new TurnAngle(90));
+    	addSequential(new Drive4Distance(140.0));
+    	addSequential(new TurnAngle(90));
+    	addSequential(new Drive4Distance(200.0));
+    	
+    }
+}
---
+++ src/org/usfirst/frc/team818/robot/commands/components/IntakeInAutonCommand.java
@@ -0,0 +1,54 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package org.usfirst.frc.team818.robot.commands.components;
+
+import org.usfirst.frc.team818.robot.commands.CommandBase;
+
+import edu.wpi.first.wpilibj.Timer;
+
+public class IntakeInAutonCommand extends CommandBase {
+	
+	Timer timer;
+	
+	private double speed, time;
+	
+	public IntakeInAutonCommand(double speed) {
+		requires(intake);
+		timer = new Timer();
+		this.speed = speed;
+		this.time = 3;
+	}
+	
+	public IntakeInAutonCommand(double speed, double time) {
+		requires(intake);
+		timer = new Timer();
+		this.speed = speed;
+		this.time = time;		
+	}
+
+	protected void initialize() {
+		timer.start();
+    	intake.intakeOff();
+	}
+
+	protected void execute() {
+		intake.intakeIn(speed);
+	}
+
+	protected boolean isFinished() {
+		return timer.hasPeriodPassed(time);
+	}
+
+	protected void end() {
+		intake.intakeOff();
+	}
+
+	protected void interrupted() {
+		intake.intakeOff();
+	}
+}
