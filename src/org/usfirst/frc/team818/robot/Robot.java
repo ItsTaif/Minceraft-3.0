ddfb9e2095e6551f0f1d242bb4d65276610253d8
BrianTeam818 <le.danh.brian@gmail.com>
2018-03-09 15:07:24 -0500

    Iterative Robot
    Switched Robot.java to IteratitiveRobot

--- src/org/usfirst/frc/team818/robot/Robot.java
+++ src/org/usfirst/frc/team818/robot/Robot.java
@@ -1,83 +1,57 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
 
 package org.usfirst.frc.team818.robot;
 
 import org.usfirst.frc.team818.robot.commands.CommandBase;
+import org.usfirst.frc.team818.robot.utilities.GetGameData;
 import org.usfirst.frc.team818.robot.utilities.RobotLog;
 
-import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 
-public class Robot extends TimedRobot {
+public class Robot extends IterativeRobot {
 
-	Command m_autonomousCommand;
-	SendableChooser<Command> m_chooser = new SendableChooser<>();
+	Command autonomous;
+	GetGameData gameData;
 
 	public void robotInit() {
-		
 		CommandBase.init();
 		RobotLog.init();
-//		chooser.addObject("My Auto", new MyAutoCommand());
-//		SmartDashboard.putData("Auto mode", m_chooser);
-		
-	}
-
-	public void disabledInit() {
-
-	}
-
-	public void disabledPeriodic() {
-		Scheduler.getInstance().run();
+		gameData.start();
 	}
 
 	public void autonomousInit() {
-		m_autonomousCommand = AutonomousSelector.getSelectedAutonomous();
-//		m_autonomousCommand = new Baseline();
-		/*
-		 * String autoSelected = SmartDashboard.getString("Auto Selector",
-		 * "Default"); switch(autoSelected) { case "My Auto": autonomousCommand
-		 * = new MyAutoCommand(); break; case "Default Auto": default:
-		 * autonomousCommand = new ExampleCommand(); break; }
-		 */
 
-		// schedule the autonomous command (example)
-		if (m_autonomousCommand != null) {
-			m_autonomousCommand.start();
-		}
+		autonomous = AutonomousSelector.getSelectedAutonomous();
+		if (autonomous != null)
+			autonomous.start();
+
 	}
 
 	public void autonomousPeriodic() {
-		/*SmartDashboard.putNumber("Gyro", CommandBase.drive.getAngle());
-		SmartDashboard.putString("RobotLog", "L:" + CommandBase.drive.getLeftRotation()+" R:"+CommandBase.drive.getRightRotation());
-		System.out.println("Gyro: " +  CommandBase.drive.getAngle());
-		System.out.println("L: " + CommandBase.drive.getLeftRotation()+" R: "+CommandBase.drive.getRightRotation());
-		*/
 		Scheduler.getInstance().run();
 	}
 
 	public void teleopInit() {
-		if (m_autonomousCommand != null) {
-			m_autonomousCommand.cancel();
-		}
-		//RobotLog.putMessage("Delay is " + SmartDashboard.getNumber("Delay", 0.0));
-		//System.out.println("Delay is " + SmartDashboard.getNumber("Delay", 0.0));
+		if (autonomous != null)
+			autonomous.cancel();
 	}
 
 	public void teleopPeriodic() {
-		/*SmartDashboard.putNumber("Gyro", CommandBase.drive.getAngle());
-		SmartDashboard.putString("RobotLog", "L:" + CommandBase.drive.getLeftRotation()+" R:"+CommandBase.drive.getRightRotation());
-		SmartDashboard.putString("RobotLog", "LEft" +CommandBase.drive.getPIDOutputSpeedLimitLeft());
-		SmartDashboard.putString("RobotLog", "RIght" +CommandBase.drive.getPIDOutputSpeedLimitRight());
-		*/
-		
-		//RobotLog.putMessage("Current: " + CommandBase.intake.getWristCurrent()); 
+		/**
+		 * This "Scheduler" object keeps track of which Commands need to be run by the
+		 * various subsystems on the robot. By telling the "Scheduler" to run
+		 * periodically during teleop, it is telling the robot to constantly execute all
+		 * necessary commands
+		 */
+		Scheduler.getInstance().run();
+	}
+
+	public void disabledInit() {
+		CommandBase.disable();
+	}
+
+	public void disabledPeriodic() {
 		Scheduler.getInstance().run();
 	}
 
