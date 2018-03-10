3add83cb0d89dea339b25a975c988db588a80122
BrianTeam818 <le.danh.brian@gmail.com>
2018-03-09 21:01:10 -0500

    SimpleSwitch changes
    Uses GetGameData thread now

--- src/org/usfirst/frc/team818/robot/autonomi/SimpleSwitchLeft.java
+++ src/org/usfirst/frc/team818/robot/autonomi/SimpleSwitchLeft.java
@@ -4,8 +4,8 @@ import org.usfirst.frc.team818.robot.commands.ElevatorForTimeCommand;
 import org.usfirst.frc.team818.robot.commands.IntakeDownForTime;
 import org.usfirst.frc.team818.robot.commands.IntakeOutForTimeCommand;
 import org.usfirst.frc.team818.robot.commands.components.Drive4Distance;
+import org.usfirst.frc.team818.robot.utilities.GetGameData;
 
-import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
 public class SimpleSwitchLeft extends CommandGroup {
@@ -13,7 +13,7 @@ public class SimpleSwitchLeft extends CommandGroup {
 	public SimpleSwitchLeft() {
 
 		addSequential(new Drive4Distance(32));
-		if (DriverStation.getInstance().getGameSpecificMessage().charAt(0) == 'L') {
+		if (GetGameData.getGameData().charAt(0) == 'L') {
 			addSequential(new IntakeDownForTime(0.3));
 			addSequential(new ElevatorForTimeCommand(1));
 			addSequential(new IntakeOutForTimeCommand(1));
--- src/org/usfirst/frc/team818/robot/autonomi/SimpleSwitchRight.java
+++ src/org/usfirst/frc/team818/robot/autonomi/SimpleSwitchRight.java
@@ -4,8 +4,8 @@ import org.usfirst.frc.team818.robot.commands.ElevatorForTimeCommand;
 import org.usfirst.frc.team818.robot.commands.IntakeDownForTime;
 import org.usfirst.frc.team818.robot.commands.IntakeOutForTimeCommand;
 import org.usfirst.frc.team818.robot.commands.components.Drive4Distance;
+import org.usfirst.frc.team818.robot.utilities.GetGameData;
 
-import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
 public class SimpleSwitchRight extends CommandGroup {
@@ -13,7 +13,7 @@ public class SimpleSwitchRight extends CommandGroup {
 	public SimpleSwitchRight() {
 
 		addSequential(new Drive4Distance(32));
-		if (DriverStation.getInstance().getGameSpecificMessage().charAt(0) == 'R') {
+		if (GetGameData.getGameData().charAt(0) == 'R') {
 			addSequential(new IntakeDownForTime(0.3));
 			addSequential(new ElevatorForTimeCommand(1));
 			addSequential(new IntakeOutForTimeCommand(1));
